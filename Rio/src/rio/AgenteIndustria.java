8ed303ff4afd054d56dcfaac7c3d74aa96591609
Hoturan <davidwcorral@hotmail.com>
2018-06-15 13:40:06 +0200

    Added WaWo and did something with the river

--- Rio/src/rio/AgenteIndustria.java
+++ Rio/src/rio/AgenteIndustria.java
@@ -5,12 +5,77 @@
  */
 package rio;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author David
  */
 public class AgenteIndustria extends Agent{
+    String message="Have not found one of the two basic Agents";
+    DFAgentDescription template = new DFAgentDescription();
+    ServiceDescription templateSd = new ServiceDescription();
+    
+    AID AIDrio;
+    List AIDDepuradoras = new ArrayList();
+    List AIDIndustrias = new ArrayList();
+
+     public class SearchDepuradoraAndRioOneShotBehaviour extends OneShotBehaviour
+    {
+        public SearchDepuradoraAndRioOneShotBehaviour()
+        {
+             
+        }
+ 
+        public void onStart()
+        {
+             
+        }
+ 
+        public int onEnd()
+        {
+             
+            return 1;
+        }
+ 
+        public void action()
+        {
+            templateSd.setType("AgenteRio");
+            template.addServices(templateSd);
+            SearchConstraints sc = new SearchConstraints();
+            DFAgentDescription[] results = null;
+            
+            try {
+                results = DFService.search(myAgent, template, sc );
+            } catch (FIPAException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            if (results.length > 0) {
+                DFAgentDescription dfd = results[0];
+                AID provider = dfd.getName();
+                message = "AgenteRio is "+provider;
+                AIDrio = provider;
+            }
+            System.out.println(message);
+        }
+ 
+    }
+    
+    protected void setup()
+    {
+         SearchDepuradoraAndRioOneShotBehaviour b = new SearchDepuradoraAndRioOneShotBehaviour();
+         this.addBehaviour(b); 
+    }
     
 }
--- Rio/src/rio/AgenteRio.java
+++ Rio/src/rio/AgenteRio.java
@@ -1,16 +1,114 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rio;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.util.Logger;
+ 
+ 
+public class AgenteRio extends Agent
+{
 
-/**
- *
- * @author David
- */
-public class AgenteRio extends Agent{
+    private Logger myLogger = Logger.getMyLogger(getClass().getName());
+
+    private class RioTickerBehaviour extends TickerBehaviour    {
+        String message;
+        int count_chocula;
+
+        public RioTickerBehaviour(Agent a, long period) {
+            super(a, period);
+        }
+
+ 
+        public void onStart()
+        {
+            this.message = "Agent " + myAgent +" with RioTickerBehaviour in action!!" + count_chocula;
+            count_chocula = 0;
+        }
+ 
+        public int onEnd()
+        {
+            System.out.println("I have done " + count_chocula + " iterations");
+            return count_chocula;
+        }
+        
+        public void onTick(){
+            
+            avanzaCursoAgua();
+        }
+        
+        public void avanzaCursoAgua() {
+            System.out.println("Agua avanza curso (No implementado)");
+        
+        }      
+ 
+    }
+
+    private class WaitMessageAndReplyBehaviour extends CyclicBehaviour {
+ 
+        public WaitMessageAndReplyBehaviour(Agent a) {
+            super(a);
+        }
+ 
+        public void action() {
+            ACLMessage  msg = myAgent.receive();
+            if(msg != null){
+                ACLMessage reply = msg.createReply();
+ 
+                if(msg.getPerformative()== ACLMessage.REQUEST){
+                    String content = msg.getContent();
+                    
+                    if ((content != null) && (content.indexOf("ping") != -1)){
+                        myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Received PING Request from "+msg.getSender().getLocalName());
+                        reply.setPerformative(ACLMessage.INFORM);
+                        reply.setContent("pong");
+                    }
+                    else{
+                        myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Unexpected request ["+content+"] received from "+msg.getSender().getLocalName());
+                        reply.setPerformative(ACLMessage.REFUSE);
+                        reply.setContent("( UnexpectedContent ("+content+"))");
+                    }
+ 
+                }
+                else {
+                    myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Unexpected message ["+ACLMessage.getPerformative(msg.getPerformative())+"] received from "+msg.getSender().getLocalName());
+                    reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+                    reply.setContent("( (Unexpected-act "+ACLMessage.getPerformative(msg.getPerformative())+") )");   
+                }
+                send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    } 
     
+    protected void setup()
+    {
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();   
+        sd.setType("RioAgent"); 
+        sd.setName(getName());
+        sd.setOwnership("TILAB");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this,dfd);
+            WaitMessageAndReplyBehaviour RioMessageBehaviour = new  WaitMessageAndReplyBehaviour(this);
+            this.addBehaviour(RioMessageBehaviour);
+        } catch (FIPAException e) {
+            myLogger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
+            doDelete();
+        }
+        
+        RioTickerBehaviour b = new RioTickerBehaviour(this, 3000);
+        this.addBehaviour(b);
+    }
 }
