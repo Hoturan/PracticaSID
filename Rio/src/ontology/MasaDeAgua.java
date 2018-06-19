7d7089c98176efe83ec94f8f6f30778e6bb4190a
Raúl García <raulgfuentesyt@gmail.com>
2018-06-19 15:18:57 +0200

    Masa de agua & Rio (def)

--- Rio/src/ontology/MasaDeAgua.java
+++ Rio/src/ontology/MasaDeAgua.java
@@ -2,13 +2,13 @@ package ontology;
 
 public class MasaDeAgua{
 	
-	private int volumen;					// Millon/es de litros
-	private float solidosEnSuspension; 			// mg/l
-	private float demandaBiologicaDeOxigeno;                // mg/l
-	private float demandaQuimicaDeOxigeno;                  // mg/l
-	private float totalNitratos;				// mg/l
-	private float totalSulfatos;				// mg/l
-	private int posicion;
+    private int volumen;					// Millon/es de litros
+    private float solidosEnSuspension; 			// mg/l
+    private float demandaBiologicaDeOxigeno;                // mg/l
+    private float demandaQuimicaDeOxigeno;                  // mg/l
+    private float totalNitratos;				// mg/l
+    private float totalSulfatos;				// mg/l
+    private int posicion;
 
     public int getVolumen() {
         return volumen;
@@ -66,47 +66,47 @@ public class MasaDeAgua{
         this.posicion = posicion;
     }
 
-	public MasaDeAgua(){
-		solidosEnSuspension = demandaQuimicaDeOxigeno = demandaBiologicaDeOxigeno = 
-			totalSulfatos = totalNitratos = 0.0f;
-		volumen = 1;
-		posicion = 0;
-	}
+    public MasaDeAgua(){
+            solidosEnSuspension = demandaQuimicaDeOxigeno = demandaBiologicaDeOxigeno = 
+                    totalSulfatos = totalNitratos = 0.0f;
+            volumen = 1;
+            posicion = 0;
+    }
 
-	public void contaminaAgua(){
-		this.solidosEnSuspension += 50.0f;
-		this.demandaBiologicaDeOxigeno += 50.0f;
-		this.demandaQuimicaDeOxigeno += 50.0f;
-		this.totalNitratos += 50.0f;
-		this.totalSulfatos += 50.0f;
-	}
+    public void contaminaAgua(){
+            this.solidosEnSuspension += 50.0f;
+            this.demandaBiologicaDeOxigeno += 50.0f;
+            this.demandaQuimicaDeOxigeno += 50.0f;
+            this.totalNitratos += 50.0f;
+            this.totalSulfatos += 50.0f;
+    }
 
-	public void descontaminaAgua(){
-		this.solidosEnSuspension -= 50.0f;
-		this.demandaBiologicaDeOxigeno -= 50.0f;
-		this.demandaQuimicaDeOxigeno -= 50.0f;
-		this.totalNitratos -= 50.0f;
-		this.totalSulfatos -= 50.0f;
-	}
+    public void descontaminaAgua(){
+            this.solidosEnSuspension -= 50.0f;
+            this.demandaBiologicaDeOxigeno -= 50.0f;
+            this.demandaQuimicaDeOxigeno -= 50.0f;
+            this.totalNitratos -= 50.0f;
+            this.totalSulfatos -= 50.0f;
+    }
 
-	public void mezclaMasasDeAgua(MasaDeAgua m){
-		this.volumen += m.volumen;
-		this.solidosEnSuspension += m.solidosEnSuspension;
-		this.demandaQuimicaDeOxigeno += m.demandaBiologicaDeOxigeno;
-		this.demandaQuimicaDeOxigeno += m.demandaQuimicaDeOxigeno;
-		this.totalNitratos += m.totalNitratos;
-		this.totalSulfatos += m.totalSulfatos;
-	}
+    public void mezclaMasasDeAgua(MasaDeAgua m){
+            this.volumen += m.volumen;
+            this.solidosEnSuspension += m.solidosEnSuspension;
+            this.demandaQuimicaDeOxigeno += m.demandaBiologicaDeOxigeno;
+            this.demandaQuimicaDeOxigeno += m.demandaQuimicaDeOxigeno;
+            this.totalNitratos += m.totalNitratos;
+            this.totalSulfatos += m.totalSulfatos;
+    }
 
-	public boolean aguaLimpia(){
-		return (solidosEnSuspension == 0.0f && 
-			demandaQuimicaDeOxigeno == 0.0f &&
-			demandaBiologicaDeOxigeno == 0.0f &&
-			totalNitratos == 0.0f &&
-			totalSulfatos == 0.0f);
-	}
-        
-        public void aumentarPosicion(){
+    public boolean aguaLimpia(){
+            return (solidosEnSuspension == 0.0f && 
+                    demandaQuimicaDeOxigeno == 0.0f &&
+                    demandaBiologicaDeOxigeno == 0.0f &&
+                    totalNitratos == 0.0f &&
+                    totalSulfatos == 0.0f);
+    }
+
+    public void aumentarPosicion(){
             posicion++;
         }
 }
--- Rio/src/ontology/Rio.java
+++ Rio/src/ontology/Rio.java
@@ -4,42 +4,42 @@ import java.util.List;
 
 public class Rio{
 	
-	private List<MasaDeAgua> flow;
+    private List<MasaDeAgua> flow;
 
-	public Rio(){}
+    public Rio(){}
 
-        // avanza todas las masas de agua del rio 1 posicion
-	public void avanzarCurso(){
-		for(int i = flow.size()-1; i>0; --i){
-                    MasaDeAgua mtemp = flow.get(i-1);
-                    mtemp.aumentarPosicion();
-                    flow.set(i, mtemp);
-                }
-                MasaDeAgua mNew = new MasaDeAgua();
-                flow.set(0, mNew); // al rio siempre le llega agua limpia
-	}
-        
-        
-        // devuelve un entero informando de los millones de litros extraidos
-        public int extraerAgua(int index, int volumenExtraer){
-              MasaDeAgua mtemp = flow.get(index);
-              int volumenActual = mtemp.getVolumen();
-              if(volumenActual == 0) return 0;
-              if(volumenActual >= volumenExtraer){
-                  volumenActual -= volumenExtraer;
-                  mtemp.setVolumen(volumenActual);
-                  flow.set(index, mtemp);
-                  return volumenExtraer;
-              }
-              else{
-                  mtemp.setVolumen(0);
-                  flow.set(index,mtemp);
-                  return volumenActual;
-              }
-        }
-        
-        // descarga agua al rio (teoricamente limpia)
-        public void descargarAgua(int index, MasaDeAgua masaDescarga){
+    // avanza todas las masas de agua del rio 1 posicion
+    public void avanzarCurso(){
+            for(int i = flow.size()-1; i>0; --i){
+                MasaDeAgua mtemp = flow.get(i-1);
+                mtemp.aumentarPosicion();
+                flow.set(i, mtemp);
+            }
+            MasaDeAgua mNew = new MasaDeAgua();
+            flow.set(0, mNew); // al rio siempre le llega agua limpia
+    }
+
+
+    // devuelve un entero informando de los millones de litros extraidos
+    public int extraerAgua(int index, int volumenExtraer){
+          MasaDeAgua mtemp = flow.get(index);
+          int volumenActual = mtemp.getVolumen();
+          if(volumenActual == 0) return 0;
+          if(volumenActual >= volumenExtraer){
+              volumenActual -= volumenExtraer;
+              mtemp.setVolumen(volumenActual);
+              flow.set(index, mtemp);
+              return volumenExtraer;
+          }
+          else{
+              mtemp.setVolumen(0);
+              flow.set(index,mtemp);
+              return volumenActual;
+          }
+    }
+
+    // descarga agua al rio (teoricamente limpia)
+    public void descargarAgua(int index, MasaDeAgua masaDescarga){
             MasaDeAgua mtemp = flow.get(index);
             mtemp.mezclaMasasDeAgua(masaDescarga);
             flow.set(index, mtemp);
